971f956eb4827946dd7f90f110d8e7e83e47a3ba
Jeffrey Wong <jeffreywong@192.168.1.129>
2022-11-03 02:01:53 +0000

    config

--- configs.py
+++ configs.py
@@ -8,7 +8,6 @@ DATASET_PATH_NOISY = "./intro2ML-coursework1/wifi_db/noisy_dataset.txt"
 DATASET_PATH_CLEAN = "./intro2ML-coursework1/wifi_db/clean_dataset.txt"
 DATASET = "CLEAN" # "NOISY                                                                     # DATASET to perform 10-nested cross validation w/o pruning
 RANDOM_SEED = 50012
-MAX_DEPTH = 10000
 
 
 # K_FOLDS
